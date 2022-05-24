e1b407961653367aa99b360218c67761625c36a5
Coda <a.ardroudi@gmail.com>
2022-05-24 10:26:44 +0200

    setup vs

---
+++ Endine/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stdlib.h> //standard C library
+
+#include "core/Object.h"
+
+using namespace Core;
+
+int main()
+{
+
+	int* a = new int; // int pointer on heap
+
+	*a = 2;
+	std::cout << a << std::endl;
+
+	Core::Object* obj = new Core::Object();
+
+
+	Core::Vector2 point{ 2, 2 };
+	obj->Move(point);
+
+	//    auto pos = obj->GetPosition();
+	std::cout << point.X << std::endl;
+
+	return 0;
+}
+
+
